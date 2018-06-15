658aa38bc3f45bf548d96db40b10d4248b1b33a6
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-06-14 22:09:58 -0400

    building the CLI interface

--- lib/best_icecream.rb
+++ lib/best_icecream.rb
@@ -1,11 +1,2 @@
-require "best_icecream/version"
-
-module BestIcecream
-
-  class Icecream
-    def ice_cream
-      `say good`
-    end
-
-  end
-end
+require_relative "./best_icecream/version"
+require_relative "./best_icecream/cli"
---
+++ lib/best_icecream/cli.rb
@@ -0,0 +1,15 @@
+# Our CLI controller
+
+class BestIcecream::CLI
+
+  def call
+    puts "Welcome to the World's Best Icecream Parlors!"
+
+  end
+
+end
+
+
+# 1. Sweet Jesus - Toronto, Ontario - Why get regular soft serve when you can get a pimped out cone? These over-the-top concoctions range from Birthday Cake Cone to Campfire S’mores and to Krusty the Cone. These cones are so decadent that you will probably leave on a serious sugar high.
+
+# 2. Serendipity 3 - NYC & Las Vegas - Ever wondered what it would be like to eat like a king? We’re pretty sure eating Serendipity’s Golden Opulence Sundae would be just like that. Listed in Guinness World Records as the most expensive sundae in the world at a price of $1,000, it’s topped with 23k edible gold. This sundae requires special ingredients from different parts of the world, so make sure to call them 48 hours in advance to request your very own. If shelling out $1,000 on ice cream isn’t your thing, don’t worry. They have other delicious (and cheaper) options too.
