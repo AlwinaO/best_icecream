6a48f8e577e7c44880dc664d02576bb673ae1fb9
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-06-25 22:05:42 -0400

    work on scraping self.scrape_spoon

--- lib/best_icecream/parlor.rb
+++ lib/best_icecream/parlor.rb
@@ -17,11 +17,12 @@ class BestIcecream::Icecream
   end
 
   def self.scrape_spoon
-    doc = Nokogiri::HTML(open("https://www.thrillist.com/eat/nation/best-ice-cream-shops-in-america"))
+    doc = Nokogiri::HTML(open("https://ny.eater.com/maps/best-new-ice-cream-shops-new-york-city"))
     binding.pry
-    title = doc.search(".postContent h3 a").text
-    # location =
-    url = doc.search(".postContent h3 a").first.attr("href")
+    name = doc.css("div .c-mapstack__card-hed div h1").first.text
+    location = doc.css("div .c-mapstack__address").first.text
+    phone = doc.search("div .c-mapstack__ info div .c-mapstack__phone desktop-only")
+    url = url = doc.css("div a link-icon").first
     # description =
 
 
